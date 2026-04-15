afbf31a9328ed1a20319ee3d5b94c0c7cddc2ff4
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-15 16:03:37 +1000

    created the sorting array from smallest to largest

---
+++ JSAT2/PART1/html/arraystart.js
@@ -0,0 +1,8 @@
+// creation of array and printing it to console // 
+const Newarray = [11, 5, 8, 3, 25, 16, 31, 45,14, 20]
+console.log(Newarray);
+
+
+// sorting the array in ascending order and printing it to console //
+const sortedArray = Newarray.sort((a, b) => a - b);
+console.log(sortedArray);
