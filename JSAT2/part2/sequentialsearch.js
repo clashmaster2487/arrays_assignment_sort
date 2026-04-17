f1722b133fe80acc74055b6b0798cc0e2af88627
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-17 17:17:22 +1000

    fixed up a small syntax error

--- JSAT2/part2/sequentialsearch.js
+++ JSAT2/part2/sequentialsearch.js
@@ -1,4 +1,11 @@
 // creation of the new array //
 const newArray = [84,33,42,95,16,27,68,75,19,50];
 
- 
+// sequential search function //
+function sequentialSearch(array,value){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] === value){
+            return i;
+        }
+}
+} 
