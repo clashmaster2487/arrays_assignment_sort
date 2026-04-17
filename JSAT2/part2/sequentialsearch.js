1bf2cd3f06be1f2cf6090817008f2bbfb671edec
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-17 19:04:06 +1000

    added author information and version number

--- JSAT2/part2/sequentialsearch.js
+++ JSAT2/part2/sequentialsearch.js
@@ -1,8 +1,14 @@
+// file: sequentialSearch.js
+// this file shows how linear search works 
+// Author: john safarian 
+// last modified: 17/04/2026
+
 // creation of the new array //
 const newArray = [84,33,42,95];
 
 // sequential search function //
-function sequentialSearch(array, value){
+// first requirement is done 
+function sequentialSearch(array, value){ 
     console.log(sequentialSearch([84,33,42,95], 42));
     for(let i = 0; i < array.length; i++){
         if(array[i] === value){
