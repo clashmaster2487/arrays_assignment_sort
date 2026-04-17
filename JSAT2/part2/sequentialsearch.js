fca43fc7891cd765f0080180e09a6a3054803770
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-17 18:26:19 +1000

    modifed the console.log sequential search to be more functional

--- JSAT2/part2/sequentialsearch.js
+++ JSAT2/part2/sequentialsearch.js
@@ -1,9 +1,9 @@
 // creation of the new array //
-const newArray = [84,33,42,95,16,27,68,75,19,50];
+const newArray = [84,33,42,95];
 
 // sequential search function //
 function sequentialSearch(array, value){
-    console.log("Searching for " + value + " in the array...");
+    console.log(sequentialSearch([84,33,42,95], 42));
     for(let i = 0; i < array.length; i++){
         if(array[i] === value){
             console.log("Value found at index: " + i);
