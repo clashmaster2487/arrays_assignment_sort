40b2c35214681c98703bf86f16c77fea4bb05739
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-18 15:02:33 +1000

    made the search array 4 numbers long just for simplicty sake for me at least

---
+++ JSAT2/part3/binarysearch.js
@@ -0,0 +1,37 @@
+// file: sequentialSearch.js
+// this file shows how linear search works 
+// Author: john safarian 
+// last modified: 17/04/2026
+// version 1.0.0
+
+// creation of the new array //
+const searchArray = [55,22,11,78];
+
+
+// the value we are searching for 
+let key = 99;
+
+
+
+// parameter: array: to search and key to be found 
+// creating the search function here 
+function binarySearch(array, value){ 
+    let found = -1;
+    
+    for( let i = 0; i < array.length; i++){
+        if (array[i] === value){
+            found = i;
+            break; // stops when found
+        }
+    } 
+    return found;
+}  
+
+// function calling 
+const result = sequentialSearch(searchArray, key);
+// ouputs result 
+if (result == -1){
+    console.log(`the key of ${key} was not found in the array`)
+} else {
+    console.log(`the key of ${key} was found at index ${result}`)
+}
