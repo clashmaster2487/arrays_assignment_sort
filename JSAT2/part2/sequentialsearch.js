462e1e51005e083d9bf56ddaf0121c5a692a28c3
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-18 13:14:50 +1000

    fixed up the code and made it easier for myself to read

--- JSAT2/part2/sequentialsearch.js
+++ JSAT2/part2/sequentialsearch.js
@@ -6,30 +6,23 @@
 
 // creation of the new array //
 const searchArray = [84,33,42,95];
-console.log(searchArray);
-
-
-
 
 
 // the value we are searching for 
 let key = 42;
-console.log (`key: ${key}`);
-// outputting  the key value to the console
+
+
 
 // parameter: array: to search and key to be found 
 // creating the search function here 
 function sequentialSearch(array, value){ 
-    // set the found value to -1 (not found)
-    console.log(sequentialSearch([84,33,42,95], 42));
     let found = -1;
-    console.log(`Array: ${searchArray}`);
-    console.log(`value: ${value}`);
-    for( i = 0; i < array.length; i++){
+    
+    for( let i = 0; i < array.length; i++){
         console.log(`arraySearch[${i}]: ${array[i]}`);
-        if (array[i] === value){
+        if (arraysearch[i] === value){
             found = i;
-            console.log(`Value found at index: ${found}`);
+            break;
         }
     } 
     return found;
