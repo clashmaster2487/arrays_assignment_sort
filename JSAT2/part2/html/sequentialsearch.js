19bdb116d1af0e00f0fe6cf3adf508004b0e1d1e
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-23 14:10:44 +1000

    fixed up sequential search

---
+++ JSAT2/part2/html/sequentialsearch.js
@@ -0,0 +1,39 @@
+// file: sequentialSearch.js
+// this file shows how linear search works 
+// Author: john safarian 
+// last modified: 17/04/2026
+// version 1.0.0
+
+// creation of the new array //
+const searchArray = [84,33,42,95];
+
+
+// the value we are searching for 
+let key = 84;
+console.log(`the key we are looking for is ${key}`)
+// orginal key was 45; but for the purposes of testing i had to chnage the key to 99 to show the not found case.
+
+
+// parameter: array: to search and key to be found 
+// creating the search function here 
+function sequentialSearch(array, value){ 
+    console.log('sequential search');
+    let found = -1;
+    
+    for( let i = 0; i < array.length; i++){
+        if (array[i] === value){
+            found = i;
+            break; // stops when found
+        }
+    } 
+    return found;
+}  
+
+// function calling 
+const result = sequentialSearch(searchArray, key);
+// ouputs result 
+if (result == -1){
+    console.log(`the key of ${key} was not found in the array`)
+} else {
+    console.log(`the key of ${key} was found at index ${result}`)
+}
