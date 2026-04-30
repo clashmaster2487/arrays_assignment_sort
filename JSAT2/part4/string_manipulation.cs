2a28ab2b3dadfb610f20e66f5f75850dc810c19f
clashmaster2487 <johnsafarian9@gmail.com>
2026-04-30 14:22:12 +1000

    created the another string

--- JSAT2/part4/string_manipulation.cs
+++ JSAT2/part4/string_manipulation.cs
@@ -5,3 +5,7 @@ File.WriteAllText("filename.txt", writeText);  // Create a file and write the co
 
 string readText = File.ReadAllText("filename.txt");  // Read the contents of the file
 Console.WriteLine(readText);  // Output the content
+
+string writeText = "AnotherString";  // Read the contents of the file
+string readText = File.ReadAlltext("AnotherString.txt");  // Output the content
+Console.writeline(ReadText);      
