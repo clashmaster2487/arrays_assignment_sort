2cbcdfaea17b2e4ed38fd33ede4003a299e51062
clashmaster2487 <johnsafarian9@gmail.com>
2026-05-07 14:36:49 +1000

    created the read function

--- JSAT2/part4/filehandling.js
+++ JSAT2/part4/filehandling.js
@@ -1,10 +1,17 @@
 console.log("writing to file")
-import {writefile} from 'node:fs';
-import {Buffer} from 'node:Buffer';
+import {writeFile} from 'node:fs';
+import { Buffer } from "buffer";
 
-const data = new Uint8Array(Buffer.from('Hello Node.js'));
-writeFile('message.txt', data, (err) => {
+const data = new Uint8Array(Buffer.from('hello word and hello to file writing.js'));
+writeFile('output.txt', data, (err) => {
    if (err) throw err;
    console.log('the file has been saved')
+ 
+});
 
+import {readFile} from 'node:fs';
+readFile('output.txt', 'utf8', (err, data) => {
+if (err) throw err;
+console.log(data)
+console.log(process.cwd());
 });
