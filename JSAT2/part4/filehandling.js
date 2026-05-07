959730539d548d1139d6f2827522bf5b135b29a1
clashmaster2487 <johnsafarian9@gmail.com>
2026-05-07 14:14:27 +1000

    created the writefile function

--- JSAT2/part4/filehandling.js
+++ JSAT2/part4/filehandling.js
@@ -3,3 +3,8 @@ import {writefile} from 'node:fs';
 import {Buffer} from 'node:Buffer';
 
 const data = new Uint8Array(Buffer.from('Hello Node.js'));
+writeFile('message.txt', data, (err) => {
+   if (err) throw err;
+   console.log('the file has been saved')
+
+});
