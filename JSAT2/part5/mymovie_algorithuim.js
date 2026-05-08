58393f7cbf1ba4130c81081b70a41922a8f911ab
clashmaster2487 <johnsafarian9@gmail.com>
2026-05-08 16:38:38 +1000

    added the movies to the array

--- JSAT2/part5/mymovie_algorithuim.js
+++ JSAT2/part5/mymovie_algorithuim.js
@@ -2,12 +2,18 @@
 
 
 // movie id section
-
-let movies = [{
-    movieID: 101,
-    title: "back to the future",
-    year: 1985,
-    rating: 10.0
-    
-}]
-console.log(movies);
+let movies = [];
+/*my my own refrence here we have created a helper function with
+all the properties needed for the requirements */
+function addMovie(movieID,title,year,rating) {
+    movies.push({
+    movieID,
+    title,
+    year,
+    rating,   
+    });
+}
+// addition of movies again for my own refrence 
+addMovie(101, "back to the future", 1985, 10.0);
+addMovie(102, "star wars the the empire strikes back", 1980, 10.3)
+console.log (movies)
