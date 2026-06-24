1d413cc134653678e225e0266553c06168cdbfde
clashmaster2487 <johnsafarian9@gmail.com>
2026-06-24 13:02:59 +1000

    fixed up the arrays

--- JSAT2/PART1/html/arraystart.js
+++ JSAT2/PART1/html/arraystart.js
@@ -1,26 +1,25 @@
 // creation of array and printing it to console // 
-const Newarray = [11, 5, 8, 3, 25, 16, 31, 45,14, 20]
+let  Newarray = [11, 5, 8, 3, 25, 16, 31, 45,14, 20]
 console.log(Newarray);
 
 
 // sorting the array in ascending order (smallest to largest and printing it to console) //
-const sortedArray = Newarray.sort((a, b) => a - b);
-console.log(sortedArray);
+Newarray.sort((a, b) => a - b);
 
 // inserting the numbers 19, 23, 30 into the sorted array //
-sortedArray.push(19, 23, 30);
-console.log(sortedArray);
+Newarray.push(19, 23, 30);
+console.log(Newarray);
 
 // remove two numbers 8,31 from the array //
 // number 8 here//
-const index8 = sortedArray.indexOf(8);
+const index8 = Newarray.indexOf(8);
 if (index8 !== -1) {
   sortedArray.splice(index8, 1);
 }
 // number 31 here// 
-const index31 = sortedArray.indexOf(31);
+const index31 = Newarray.indexOf(31);
 if (index31 !== -1) {
-  sortedArray.splice(index31, 1);
+  Newarray.splice(index31, 1);
 }
-console.log(sortedArray);
+console.log(Newarray);
 
