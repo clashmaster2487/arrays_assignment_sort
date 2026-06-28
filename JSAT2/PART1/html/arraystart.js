50aeb95736eb7470d9e363fde69b209a53bef31a
clashmaster2487 <johnsafarian9@gmail.com>
2026-06-28 18:10:38 +1000

    fixed up a few small mistakes

--- JSAT2/PART1/html/arraystart.js
+++ JSAT2/PART1/html/arraystart.js
@@ -1,35 +1,36 @@
 // creation of array and printing it to console // 
 let  Newarray = [11, 5, 8, 3, 25, 16, 31, 45,14, 20]
-console.log(Newarray);
+console.log(Newarray); // console logging new araay for debugging
 
 
 // sorting the array in ascending order (smallest to largest and printing it to console) //
-Newarray.sort((a, b) => a - b);
+Newarray.sort((a, b) => a - b); // intial sorting 
 
 // inserting the numbers 19, 23, 30 into the sorted array //
-Newarray.push(19, 23, 30);
-Newarray.sort((a, b) => a - b);
-console.log(Newarray);
+Newarray.push(19, 23, 30); // here we are pushing the new numbers into the sorted array 
+Newarray.sort((a, b) => a - b); // sorting to keep ascending order
+console.log(Newarray); // here we are logging the array making sure we can debug it if anythign goes wrong 
 
 // remove two numbers 8,31 from the array //
 // number 8 here//
-const index8 = Newarray.indexOf(8);
+const index8 = Newarray.indexOf(8); // here we are removing the eigth number from the array 
 if (index8 !== -1) {
-  Newarray.splice(index8, 1);
+  Newarray.splice(index8, 1); // here is the splicing 
 }
 // number 31 here// 
-const index31 = Newarray.indexOf(31);
-if (index31 !== -1) {
+const index31 = Newarray.indexOf(31); // deleeting the numbder 31 here 
+if (index31 !== -1) { 
   Newarray.splice(index31, 1);
-}
-Newarray.sort((a, b) => a - b);
-console.log(Newarray);
+} 
+
+Newarray.sort((a, b) => a - b); // sorting it again keeping the array in asending order
+console.log(Newarray); 
 
 // parameter: array: to search and key to be found 
 // creating the search function here 
-function sequentialSearch(array, value){ 
-    console.log('sequential search');
-    let found = -1;
+function sequentialSearch(array, value){  // taking the array and value here
+    console.log('sequential search'); // logging sequential search here for debugging purposes 
+    let found = -1; 
     
     for( let i = 0; i < array.length; i++){
         if (array[i] === value){
@@ -41,7 +42,7 @@ function sequentialSearch(array, value){
 }  
 
 // key 
-let value = 65;
+let value = 5;
 
 // function calling 
 const result = sequentialSearch(Newarray, value);
@@ -49,7 +50,7 @@ const result = sequentialSearch(Newarray, value);
 if (result == -1){
     console.log(`-1`)
 } else {
-    console.log(`the key of ${key} was found at index ${result}`)
+    console.log(`the target value of {index} is found `)
 }
 
 // parameter: array: to search and key to be found 
@@ -68,10 +69,10 @@ function binarySearch(array, value){
 
 
 // function calling 
-const result2 = binarySearch(array, value);
+const result2 = binarySearch(Newarray, value);
 // ouputs result 
 if (result == -1){
-    console.log(`the key of ${key} was not found in the array`)
+    console.log(``)
 } else {
-    console.log(`the key of ${key} was found at index ${result}`)
+    console.log(``)
 }
